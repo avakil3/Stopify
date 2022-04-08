3c1dfca963caf443ca3666e61a7b995cdd011673
Aagam Vakil <aagamvakil@hotmail.com>
2022-04-07 21:19:37 -0400

    removed logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,14 @@
-import { createStore, applyMiddleware,compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk,logger)),
+    applyMiddleware(thunk),
   )
 };
 
